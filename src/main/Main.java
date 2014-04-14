044c6afef6456d657ea6362fedad888854c7942c
X3N0-Life-Form <adrarsis@laposte.net>
2014-04-14 09:36:45 +0200

    fleshed out main + added test case

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,47 @@
 package main;
 
+import java.io.IOException;
+
+import parse.Parser;
+import algo.AlgorithmException;
+import algo.ArcConsistency_1;
+import algo.Backtrack;
+import core.CSP;
+
 public class Main {
+	
+	private static CSP csp;
+	private static Backtrack backtrack;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws AlgorithmException, IOException {
+		dealsWithArgs(args);
+		printRecap();
+		backtrack.start();
+		System.out.println("Result:");
+		System.out.println(csp);
+	}
 
+	private static void dealsWithArgs(String[] args) throws IOException {
+		for (int i = 0; i < args.length; i++) {
+			switch (args[i]) {
+			case "-file":
+				csp = Parser.parseCSP(args[++i]);
+				backtrack = new Backtrack(csp);
+				break;
+			case "-ac1":
+				backtrack.addBeforeAlgorithm(new ArcConsistency_1(csp));
+				break;
+			case "-ac3":
+			case "-fc":
+				System.out.println(args[i] + " not implemented.");
+				break;
+			}
+		}
+	}
+	
+	private static void printRecap() {
+		System.out.println(csp);
+		System.out.println(backtrack);
 	}
 
 }
---
+++ test/main/TestsMain.java
@@ -0,0 +1,40 @@
+package main;
+
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import algo.AlgorithmException;
+
+public class TestsMain {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test_standard() throws AlgorithmException, IOException {
+		String[] args = new String[] {
+				"-file",
+				"data/sample_csp.txt"
+		};
+		Main.main(args);
+	}
+	
+	@Test
+	public void test_ac1() throws AlgorithmException, IOException {
+		String[] args = new String[] {
+				"-file",
+				"data/sample_csp.txt",
+				"-ac1"
+		};
+		Main.main(args);
+	}
+
+}
