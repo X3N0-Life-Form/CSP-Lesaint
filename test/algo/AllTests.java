b6789e29c9f5efc463c08c5b60e237ecaae038ae
X3N0-Life-Form <adrarsis@laposte.net>
2014-04-14 10:07:11 +0200

    added test suite for algo package tests, with more voodoo fun tidbis: 	When run individually: 		TestsAC1:	greenbar 		TestsBacktrack:	redbar - each test fails 		TestsAlgorithm:	redbar - only the anonymous algo test passes 	When run as a suite: 		TestsAC1:	greenbar 		TestsBacktrack:	greenbar 		TestsAlgorithm:	redbar - only AC1_backtrack fails

---
+++ test/algo/AllTests.java
@@ -0,0 +1,11 @@
+package algo;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestsAC1.class, TestsAlgorithm.class, TestsBacktrack.class })
+public class AllTests {
+
+}
